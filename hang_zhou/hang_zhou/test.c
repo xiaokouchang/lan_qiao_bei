742bd5377c5d71f6a052540490adb068754560d7
xpr123 <3424286550@qq.com>
2025-03-23 22:06:23 +0800

    亲和数

--- hang_zhou/hang_zhou/test.c
+++ hang_zhou/hang_zhou/test.c
@@ -2895,7 +2895,7 @@
 //5
 
 
-//第2038题
+//第2039题
 //三角形
 //Problem Description
 //给定三条边,请你判断一下能不能组成一个三角形。
@@ -2956,3 +2956,82 @@
 //}
 
 
+//第2040题
+//亲和数
+//Problem Description
+//古希腊数学家毕达哥拉斯在自然数研究中发现,220的所有真约数(即不是自身的约数)之和为:
+//1 + 2 + 4 + 5 + 10 + 11 + 20 + 22 + 44 + 55 + 110＝284。
+//而284的所有真约数为1、2、4、71、142, 加起来恰好为220。
+//人们对这样的数感到很惊奇, 并称之为亲和数。
+//一般地讲, 如果两个数中任何一个数都是另一个数的真约数之和,则这两个数就是亲和数。
+//你的任务就编写一个程序, 判断给定的两个数是否是亲和数
+//Input
+//输入数据第一行包含一个数M,接下有M行,每行一个实例,
+//包含两个整数A,B;
+//其中0<=A,B<=600000;
+//Output
+//对于每个测试实例,如果A和B是亲和数的话输出YES,否则输出NO。
+//Sample Input
+//2
+//220 284
+//100 200
+//Sample Output
+//YES
+//NO
+//#include<stdio.h>
+//#define ROW 100
+//#define COL 100
+//int NUM1(int num)
+//{
+//	int i = 0;
+//	int count = 0;
+//	int sum = 0;
+//	for (i = 1;i < num - 1;i++)
+//	{
+//		if (num % i == 0)
+//		{
+//			sum += i;
+//		}
+//	}
+//	return sum;
+//}
+//void NUM(int arr1[ROW][COL], int num)
+//{
+//	int i = 0;
+//	int arr2[ROW] = { 0 };
+//	for (i = 0;i < num;i++)
+//	{
+//		int j = 0;
+//		int count = 0;
+//		for (j = 0;j < 2;j++)
+//		{
+//			int ret = NUM1(arr1[i][j]);
+//			arr2[count++] = ret;
+//		}
+//		if (arr2[0] == arr1[i][1] && arr2[1] == arr1[i][0])
+//		{
+//			printf("YES\n");
+//		}
+//		else
+//		{
+//			printf("NO\n");
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr1[ROW][COL] = { 0 };
+//	int num = 0;
+//	scanf("%d", &num);
+//	int i = 0;
+//	for (i = 0;i < num;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < 2;j++)
+//		{
+//			scanf("%d", &arr1[i][j]);
+//		}
+//	}
+//	NUM(arr1, num);
+//	return 0;
+//}
